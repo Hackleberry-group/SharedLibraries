bfc7f78ef5ff7c3e0c5e2ad8cd25642eb2713b01
Vedat <683343@student.inholland.nl>
2024-12-05 11:53:14 +0100

    cleaning up

--- Exceptions/ApiExceptionHandler.cs
+++ Exceptions/ApiExceptionHandler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http.ExceptionHandling;
+﻿using System.Web.Http.ExceptionHandling;
 
 namespace HackleberrySharedModels.Middleware
 {
