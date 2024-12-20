d32fe34483e0d98bc7c2d92687be59fa95d9b9b8
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2024-12-20 16:56:26 +0100

    added StudentStatResponse and LevelResponse

---
+++ HackleberrySharedModels/Responses/LevelResponse.cs
@@ -0,0 +1,11 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace HackleberryModels.Responses
+{
+    public class LevelResponse
+    {
+        public int LevelNumber { get; set; }
+        public int RequiredXP { get; set; }
+    }
+}
---
+++ HackleberrySharedModels/Responses/StudentStatResponse.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace HackleberryModels.Responses
+{
+    public class StudentStatResponse
+    {
+        public Guid StudentId { get; set; }
+        public int TotalNumberOfAnswersCorrect { get; set; }
+        public int TotalNumberOfAnswersIncorrect { get; set; }
+        public int TotalNumberOfExercisesCompleted { get; set; }
+        public int TotalXP { get; set; }
+        public int Level { get; set; }
+        public int StreakNumber { get; set; }
+        public bool StreakFreezeAvailable { get; set; }
+
+        public string LastStreakDay { get; set; }
+    }
+}
