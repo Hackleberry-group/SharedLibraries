81dbdec02367f96425ac27d9c076904e35548ff3
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2024-12-20 11:38:56 +0100

    added single stat request

---
+++ HackleberrySharedModels/Requests/SingleStatRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HackleberrySharedModels.Models
+{
+    public class SingleStatRequest
+    {
+        // public Guid StudentId { get; set; }
+        public Guid ExerciseId { get; set; }
+        public bool IsExerciseCompleted { get; set; }
+        public int NumberOfAnswersCorrect { get; set; }
+        public int NumberOfAnswersIncorrect { get; set; }
+    }
+}
