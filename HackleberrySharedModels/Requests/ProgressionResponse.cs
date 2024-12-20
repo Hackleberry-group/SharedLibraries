013689f0a817296aaf94d41102e9d8ddf38c873d
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2024-12-20 11:53:20 +0100

    added ProgressionResponse

---
+++ HackleberrySharedModels/Requests/ProgressionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace HackleberrySharedModels.Requests
+{
+    public class ProgressionResponse
+    {
+        public Guid StudentId { get; set; }
+        public double ProgressPercentage { get; set; }
+        public List<ExerciseState> ExerciseStates { get; set; }
+    }
+}
