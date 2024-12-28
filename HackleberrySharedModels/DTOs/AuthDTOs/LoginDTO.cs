0eea8d873d10aa8f86ea1d28e65af004840817b2
Joshua Andrea <joshua.andrea@hotmail.com>
2024-12-28 12:32:54 +0100

    login DTO

--- HackleberrySharedModels/DTOs/AuthDTOs/LoginDTO.cs
+++ HackleberrySharedModels/DTOs/AuthDTOs/LoginDTO.cs
@@ -1,7 +1,14 @@
-﻿namespace HackleberryModels.DTOs.AuthDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HackleberryModels.DTOs.AuthDTOs
 {
     public record LoginDTO
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Email is not valid.")]
+        public string Email { get; init; }
 
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; init; }
     }
 }
