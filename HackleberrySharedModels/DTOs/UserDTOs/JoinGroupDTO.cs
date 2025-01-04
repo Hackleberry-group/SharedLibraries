c153926f1f7da74f2f7e5f65e3c63024e20aaeb9
Joshua Andrea <joshua.andrea@hotmail.com>
2025-01-04 11:35:57 +0100

    Rename joingroupdto

--- HackleberrySharedModels/DTOs/UserDTOs/JoinGroupDTO.cs
+++ HackleberrySharedModels/DTOs/UserDTOs/JoinGroupDTO.cs
@@ -2,9 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HackleberrySharedModels.DTOs.UserDTOs;
 
-public class CreateStudentGroupDTO
+public class JoinGroupDTO
 { 
     [Required(ErrorMessage = "Join code is required.")]
     [StringLength(4, ErrorMessage = "Join code must be 4 characters long.")]
     public string JoinCode { get; set; }
+
 }
