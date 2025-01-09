33fc2204ee4f465c91bd7b5b6d085e6924dd23d7
Joshua Andrea <joshua.andrea@hotmail.com>
2025-01-09 19:35:17 +0100

    create new CourseProgressionDTOs

---
+++ HackleberrySharedModels/DTOs/CourseProgressionDTOs/CourseProgressionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HackleberryModels.DTOs.CourseProgressionDTOs
+{
+    public record CourseProgressionDTO
+    {
+        public Guid StudentId { get; init; }
+        public Guid CourseId { get; init; }
+        public string CourseName { get; init; }
+        public string ProgrammingLanguage { get; init; }
+        public int ProgressionPercentage { get; init; }
+        public List<ModuleProgressionDTO> ModuleProgressions { get; init; } = new List<ModuleProgressionDTO>();
+    }
+}
---
+++ HackleberrySharedModels/DTOs/CourseProgressionDTOs/ModuleProgressionDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HackleberryModels.DTOs.CourseProgressionDTOs
+{
+    public record ModuleProgressionDTO
+    {
+        public Guid ModuleId { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public int ProgressionPercentage { get; set; }
+        public IEnumerable<TopicProgressionDTO> TopicProgressions { get; set; } = new List<TopicProgressionDTO>();
+    }
+}
