89f47141d72c61a6a0a0e4e104e4b14eaf9ee6b1
Joshua Andrea <joshua.andrea@hotmail.com>
2025-01-12 12:42:29 +0100

    add xp-to-next-level property to student stats dto

--- HackleberrySharedModels/DTOs/ProgressionServiceDTOs/StudentStatResponseDTO.cs
+++ HackleberrySharedModels/DTOs/ProgressionServiceDTOs/StudentStatResponseDTO.cs
@@ -8,6 +8,7 @@
         public int TotalNumberOfExercisesCompleted { get; set; }
         public int TotalXP { get; set; }
         public int Level { get; set; }
+        public int XPToNextLevel { get; set; }
         public int StreakNumber { get; set; }
         public bool StreakFreezeAvailable { get; set; }
         public DateTime LastStreakDay { get; set; }
