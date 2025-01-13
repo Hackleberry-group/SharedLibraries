cf270e1e77846d302b180331b99539ef48abdc4d
Joshua Andrea <joshua.andrea@hotmail.com>
2025-01-13 15:27:15 +0100

    applicationdbconfig change

---
+++ HackleberrySharedModels/DTOs/UserDTOs/LinkStudentRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HackleberryModels.DTOs.UserDTOs;
+
+public record LinkStudentRequestDTO
+{
+    [Required]
+    public Guid StudentId { get; init; }
+}
