25c3506b0b9e7784b209599a5f0f423c1aefaa45
Joshua Andrea <joshua.andrea@hotmail.com>
2025-01-14 18:19:05 +0100

    add refresh token to login response

--- HackleberrySharedModels/DTOs/AuthDTOs/LoginResponseDTO.cs
+++ HackleberrySharedModels/DTOs/AuthDTOs/LoginResponseDTO.cs
@@ -5,8 +5,6 @@ namespace HackleberryModels.DTOs.AuthDTOs
     public record LoginResponseDTO
     {
         public string AccessToken { get; init; }
-
-        //TODO: Add when refresh token is implemented
-        //public string RefreshToken { get; init; } 
+        public string RefreshToken { get; init; } 
     }
 }
