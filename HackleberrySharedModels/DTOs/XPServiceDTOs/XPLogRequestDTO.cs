10b603b2c8bddc23574584e43d272903608e7de7
Vedat <683343@student.inholland.nl>
2025-01-14 15:56:44 +0100

    add xp log request

---
+++ HackleberrySharedModels/DTOs/XPServiceDTOs/XPLogRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HackleberryModels.DTOs.XPServiceDTOs;
+
+public record XPLogRequestDTO
+{
+    public Guid StudentId { get; init; }
+
+    public int Exp { get; init; }
+
+    public Guid CourseId { get; init; }
+}
