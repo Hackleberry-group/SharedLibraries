abbc46f5285521f74a3a94adfc98169536b7b620
Joshua Andrea <joshua.andrea@hotmail.com>
2025-01-16 13:33:40 +0100

    add claims helper

---
+++ HackleberryServices/Helpers/ClaimsHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Claims;
+
+namespace HackleberryServices.Helpers;
+
+public static class ClaimsHelper
+{
+    public static Guid GetUserId(ClaimsPrincipal user)
+    {
+        var subClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        return subClaim != null ? Guid.Parse(subClaim) : Guid.Empty;
+    }
+
+    public static bool IsInRole(ClaimsPrincipal user, string role)
+    {
+        return user.IsInRole(role);
+    }
+
+    public static bool IsTeacher(ClaimsPrincipal user)
+    {
+        return IsInRole(user, "Teacher");
+    }
+
+    public static bool IsStudent(ClaimsPrincipal user)
+    {
+        return IsInRole(user, "Student");
+    }
+
+    public static bool IsAdmin(ClaimsPrincipal user)
+    {
+        return IsInRole(user, "Admin");
+    }
+
+    public static string? GetUserEmail(ClaimsPrincipal user)
+    {
+        return user.FindFirst(ClaimTypes.Email)?.Value;
+    }
+
+    public static string? GetUserName(ClaimsPrincipal user)
+    {
+        return user.FindFirst(ClaimTypes.Name)?.Value;
+    }
+}
