8975b7feca22862a5d8b0cc4d81bcdc76ab4539b
Sara-Eftekhar-Azam <90862126+Sara-Eftekhar-Azam@users.noreply.github.com>
2025-01-18 12:13:45 +0100

    added fileDTO

---
+++ HackleberrySharedModels/DTOs/FileServiceDTOs/FileDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackleberryModels.DTOs.FileServiceDTOs
+{
+    public class FileDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Url { get; set; } = null;
+        public FileDTO() { }
+        public FileDTO(Guid guid, string name) 
+        {
+            Id = guid;
+            Name = name;
+        }
+        public FileDTO(Guid guid, string name, string url) : this(guid, name)
+        {
+            Url = url;
+        }
+    }
+}
