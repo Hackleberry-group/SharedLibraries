fcbf3aa17ac85260297fac20fd3433c462b0e6a6
Vedat <683343@student.inholland.nl>
2025-01-20 14:11:24 +0100

    added annotation to the exercise and topic request

--- HackleberrySharedModels/DTOs/CourseServiceDTOs/ExerciseRequestDTO.cs
+++ HackleberrySharedModels/DTOs/CourseServiceDTOs/ExerciseRequestDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace HackleberryModels.DTOs.CourseServiceDTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HackleberryModels.DTOs.CourseServiceDTOs;
 
 public class ExerciseRequestDTO
 {
+    [Required]
     public int Order { get; set; }
 
+    [Required]
     public Guid TopicId { get; set; }
 
+    [Required]
     public bool? IsTopicExam { get; set; }
 }
--- HackleberrySharedModels/DTOs/CourseServiceDTOs/TopicRequestDTO.cs
+++ HackleberrySharedModels/DTOs/CourseServiceDTOs/TopicRequestDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace HackleberryModels.DTOs.CourseServiceDTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HackleberryModels.DTOs.CourseServiceDTOs;
 
 public class TopicRequestDTO
 {
+    [Required]
     public string Name { get; set; }
 
+    [Required]
     public Guid ModuleId { get; set; }
 
+    [Required]
     public int Order { get; set; }
 }
