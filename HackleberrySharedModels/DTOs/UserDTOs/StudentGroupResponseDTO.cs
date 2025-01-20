481375e648d38b0f5dc3b5270cd51aa51114d5a2
Joshua Andrea <joshua.andrea@hotmail.com>
2025-01-20 15:55:15 +0100

    fix join group bug

---
+++ HackleberrySharedModels/DTOs/UserDTOs/StudentGroupResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace HackleberryModels.DTOs.UserDTOs
+{
+    public class StudentGroupResponseDTO
+    {
+        public Guid StudentId { get; set; }
+        public Guid GroupId { get; set; }
+    }
+}
